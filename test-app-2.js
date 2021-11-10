ae551f95e3ec66c814c9533098c745c1fb5f14c5
xkoti <80556958+xkoti@users.noreply.github.com>
2021-11-10 17:31:24 +0800

    Update and rename test-app-1.js to test-app-2.js

--- test-app-2.js
+++ test-app-2.js
@@ -5881,6 +5881,9 @@ function append_files_to_list(path, files) {
                 <button id="copy-link-message" onclick="(function setClipboard(value) {var tempInput = document.createElement('input');tempInput.style = 'position: absolute; left: -1000px; top: -1000px';tempInput.value = value;document.body.appendChild(tempInput);tempInput.select();document.execCommand('copy');document.body.removeChild(tempInput);})(window.location.protocol + '//' + window.location.hostname + '${p}')" class="mdui-textfield-icon mdui-btn mdui-btn-icon dummyclass" style="float: right;">
                   <i class="mdui-icon material-icons dummyclass">content_copy</i>
                 </button>
+		<button id="copy-link">
+    		  <i class="mdui-icon material-icons dummyclass">adb</i>
+    		</button>		
               </div>
 	        </li>`;
     } else {
